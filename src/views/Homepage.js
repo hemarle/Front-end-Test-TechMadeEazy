0d02f50791e474b86ab4cd2bc7d5f75db80a795e
Daramola Femi <dfmopeoluwa@gmail.com>
2022-04-15 03:36:14 +0100

    fixed netlify issues

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -1,9 +1,5 @@
 import React, { useEffect } from "react";
 
-import Card from "../components/Card/Card";
-import Sidebar from "../components/Sidebar/Sidebar";
-import image from "../images/image.png";
-import avatar from "../images/author.png";
 import "../styles/homepage.css";
 import Footer from "../components/Footer/Footer";
 
@@ -13,35 +9,32 @@ import Content from "../components/Content/Content";
 
 function Homepage() {
   const cards = useSelector((state) => state.cards);
-  const tester = useSelector((state) => state.cards);
 
   const dispatch = useDispatch();
 
   const fetchData = () => {
-    let result = axios
+    let newData = [];
+    axios
       .get(
         "https://api.terawork.com/service-categories/sellers-services/computer-software-development"
       )
-      .then((data) =>
+      .then((data) => {
+        data.data.data.service_search_results.hits.forEach((data) =>
+          newData.push({ style: "", name: data })
+        );
+
         dispatch({
           type: "ADD_PRODUCTS",
-          payload: data.data.data.service_search_results.hits,
+          payload: newData,
           style: "",
-        })
-      )
+        });
+      })
       .catch((err) => console.log(err));
-
-    // let resultData = result.data.data.service_search_results.hits;
-
-    // resultData.map((data) =>
-    // );
   };
 
   useEffect(() => {
     fetchData();
   }, []);
-
-  console.log(tester, "testers");
 
   return (
     <div className="homepage">
@@ -50,10 +43,5 @@ function Homepage() {
     </div>
   );
 }
-// function mapStateToProps(state) {
-//   console.log(state.favourites);
-//   return { cards: state.cards };
-// }
 
-// export default connect(mapStateToProps)(homepage);
-export default Homepage;
+export default React.memo(Homepage);
